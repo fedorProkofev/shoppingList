6d8f4d61279bc86b1663b01be9ba6c6c30089df2
Fyodor Prokofiev <9195737+fprokofiev@users.noreply.github.com>
2019-10-26 20:41:30 +0300

    update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul#sl-list");
-var lis = document.querySelectorAll("li.sl-list-item");
+// Vars for global scope
+let button = document.getElementById("enter");
+let input = document.getElementById("userinput");
+let ul = document.querySelector("ul#sl-list");
+let lis = document.querySelectorAll("li.sl-list-item");
 
-function inputLength() {
-	return input.value.length;
-}
+// Functions
+const inputLength = () => input.value.length;
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.innerHTML = "<span class=\"sl-list-content\">" + input.value + "</span> <button class=\"btn btn-sm btn-danger list-button-del\">Delete</button>";
+const createListElement = () => {
+	let li = document.createElement("li");
+	li.innerHTML = `<span class="sl-list-content">${input.value}</span> <button class="btn btn-sm btn-danger list-button-del">Delete</button>`;
 	li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center", "sl-list-item");
 	deleteListElement(li);
 	doneListElement(li);
@@ -17,35 +17,34 @@ function createListElement() {
 	input.value = "";
 }
 
-function doneListElement(element) {
+const doneListElement = (element) => {
 	element.querySelector(".sl-list-content").addEventListener("click", function() {
 		element.classList.toggle("done");
 	});
 }
 
-function deleteListElement(element) {
+const deleteListElement = (element) => {
 	element.querySelector(".list-button-del").addEventListener("click", function() {
 		element.remove(element);
 	});
 }
 
-function addListAfterClick() {
+const addListAfterClick = () => {
 	if (inputLength() > 0) {
 		createListElement();
 	}
 }
 
-function addListAfterKeypress(event) {
+const addListAfterKeypress = (event) => {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
 	}
 }
 
+// Events
 button.addEventListener("click", addListAfterClick);
-
 input.addEventListener("keypress", addListAfterKeypress);
-
-lis.forEach(function(element) {
+for (let element of lis) {
 	doneListElement(element);
 	deleteListElement(element);
-});
+}
